6750ab438a73700cd1f6f12b5b1ce1e23d8880ca
mitch7553-ctrl <mitch7553@gmail.com>
2026-05-07 18:34:53 +0800

    Will work on the rest of this at home

---
+++ bigO_exercise3.js
@@ -0,0 +1,15 @@
+// Big O rule 3 
+
+function comopressBoxesTwice(boxes，boxes2) {
+    boxes.forEach(function(box) {
+        console.log(box);
+    });
+
+    boxes2.forEach(function(box) {
+        console.log(box);
+    }));
+}       
+
+
+
+//0(n)
