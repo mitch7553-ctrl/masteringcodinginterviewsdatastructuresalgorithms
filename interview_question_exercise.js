c992aa60b9afd58c938c3f567c24ceda7fe1ca5c
Mitchell Brown <mitch7553@gmail.com>
2026-05-08 10:40:00 +0800

    Working through the coding interview section

---
+++ interview_question_exercise.js
@@ -0,0 +1,13 @@
+//Givve 2 arrays, create a function that let's user know (true/false) 
+// whether these tow arrays contain any common items 
+//For Example:
+//const array1 = ['a', 'b', 'c', 'x'];
+// const array2 ['z', 'y', 'i'];
+//should return flase .
+//-------- 
+//const array1 = ['a', 'b', 'c', 'x'];
+// const array2 ['z', 'y', 'x'];
+//should return true 
+
+// 2 parameters - arrays 
+// return true or false 
