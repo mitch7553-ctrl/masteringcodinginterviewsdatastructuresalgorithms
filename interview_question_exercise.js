0cc363328aaff97a2e2a3cf4e830e75e1116ef50
mitch7553-ctrl <mitch7553@gmail.com>
2026-05-09 13:24:18 +0800

    Finished the Big o and coding section

--- interview_question_exercise.js
+++ interview_question_exercise.js
@@ -11,3 +11,21 @@
 
 // 2 parameters - arrays 
 // return true or false 
+const array1 = ['a', 'b', 'c', 'x'];
+const array2 = ['z', 'y', 'x'];
+function containsCommonItem(array1, array2)  {
+    for (let i=0; i < array1.length; i++) {
+        for (let j=0; j < array2.length; j++) {
+            if (array1[i] === array2[j]) {
+                return true;
+            }
+
+            }
+        }
+         return false 
+    }
+   
+
+
+containsCommonItem(array1, array2)
+//0(n^2)
