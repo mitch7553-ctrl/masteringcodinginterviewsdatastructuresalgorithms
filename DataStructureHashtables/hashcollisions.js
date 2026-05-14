b0899ac6cf01a109cc9a0af2b630b809a45469db
mitch7553-ctrl <mitch7553@gmail.com>
2026-05-14 13:48:09 +0800

    working through linkedlists .

---
+++ DataStructureHashtables/hashcollisions.js
@@ -0,0 +1,13 @@
+let user = {
+    age: 54, 
+    name: 'testname',
+    magic: true,
+    scream: function() {
+        console.log('ahhhhhhhhhhh');
+    }
+}
+
+
+console.log(user.age)
+user.spell = 'ajsjsjhshhshs';
+user.scream();
---
+++ DataStructureLinkedList/linkedlinstintro.js
@@ -0,0 +1,11 @@
+//What is a linked list?
+const basket = ['apples', 'grapes', 'pears']
+
+//linked list: apples --> grapes -> pears
+
+//apples 
+//8947 --> grapes
+//        8742  --> pears
+//                    372 --> null
+
+                        
