ea73159fed6cf6643e738bca7dd4de70124e18a2
mitch7553-ctrl <mitch7553@gmail.com>
2026-05-16 01:12:45 +0800

    working through search section

---
+++ AlgorithmsSearching/linearSearch.js
@@ -0,0 +1,15 @@
+//Linear Searching 
+const beasts = ["Centaur", "Godzilla", "Mosura", "Minotaur", "Hydra", "Nessie"];
+
+
+beasts.indexOf("Godzilla");
+
+beasts.findIndex((item) => {
+    return item === "Godzilla";
+});
+
+beasts.find((item) => {
+    return item === "Godzilla";
+});
+
+beasts.includes("Godzilla");
